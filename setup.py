2425112c35f6b9756704665e1ec37daf4f37b787
Arkodeep Sen <arkodeepsen72@gmail.com>
2024-11-20 14:39:31 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,42 @@
 import os
 from setuptools import setup, find_packages
 
+import os
+from setuptools import setup, find_packages
+
+try:
+    with open('README.md') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = '''# OpenAPI Gen Wrapper
+[![Upload Python Package](https://github.com/arkodeepsen/openapi-gen-wrapper/actions/workflows/python-publish.yml/badge.svg)](https://github.com/arkodeepsen/openapi-gen-wrapper/actions/workflows/python-publish.yml)
+## Description
+
+A Python wrapper for generating OpenAPI specifications from routes in a Python project.
+
+## Installation
+
+```bash
+pip install openapi-gen-wrapper
+```
+
+## Usage
+Here’s how you can use the wrapper:
+
+```python
+from openapi_gen_wrapper import generate_openapi_spec
+```
+
+# Example usage
+```
+generate_openapi_spec(routes=["/hello", "/goodbye"])
+```
+License
+```
+MIT License
+```
+'''
+
 setup(
     name="openapi-gen-wrapper",
     version="0.1.0",
@@ -14,7 +50,7 @@ setup(
     author="Arkodeep Sen",
     author_email="arkodeepsen72@gmail.com.com",
     description="A Python tool for generating OpenAPI specs",  # Short description
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",  # The format of your README
     url="https://github.com/arkodeepsen/openapi-gen",
     classifiers=[
